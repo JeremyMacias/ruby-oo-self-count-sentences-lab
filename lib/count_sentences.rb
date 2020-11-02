02ffc435d5c8d6bce08adac10b2d23c94d5f1741
Jeremy Macias <jeremypmacias@gmail.com>
2020-11-02 11:08:44 -0700

    commit tree 8ba4d22d729d1bceb55d99986c4534a239472e8a

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,38 @@ require 'pry'
 class String
 
   def sentence?
-    
+    if self.end_with?(".")
+      return true
+    else
+      return false
+    end
   end
 
   def question?
+    if self.end_with?("?")
+      return true
+    else
+      return false
+    end
 
   end
 
   def exclamation?
+    if self.end_with?("!")
+      return true
+    else
+      return false
+    end
 
   end
 
+  # use 'self'
+  # return count of sentences with integer
+  # 'split' string on periods, question marks and exclamation marks
+  # 'count' num of elements that result from 'split'
   def count_sentences
-
+    array = self.split(/[!?.]/).reject {|string| string.empty?}
+    return array.length
   end
+  
 end
